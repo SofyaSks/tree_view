06ba2db24ae48df35842f5ea931ccfb36fa50e24
sks <ssurovneva@mail.ru>
2023-04-17 21:37:34 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace tree_view
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Root Node 1");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node1");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node2");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node5");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node4", new System.Windows.Forms.TreeNode[] {
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
+            treeNode6});
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node6");
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(52, 42);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Root Node 1";
+            treeNode2.Name = "Node1";
+            treeNode2.Text = "Node1";
+            treeNode3.Name = "Node2";
+            treeNode3.Text = "Node2";
+            treeNode4.Name = "Node0";
+            treeNode4.Text = "Node0";
+            treeNode5.Name = "Node5";
+            treeNode5.Text = "Node5";
+            treeNode6.Name = "Node4";
+            treeNode6.Text = "Node4";
+            treeNode7.Name = "Node3";
+            treeNode7.Text = "Node3";
+            treeNode8.Name = "Node6";
+            treeNode8.Text = "Node6";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode4,
+            treeNode7,
+            treeNode8});
+            this.treeView1.Size = new System.Drawing.Size(220, 227);
+            this.treeView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 597);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tree_view
+{
+    public partial class Form1 : Form
+    {
+        TreeView tree;
+        ImageList galary; // сохраняет память, но работает только для независимых окон
+        ListBox lb;
+        public Form1()
+        {
+            InitializeComponent();
+            this.SetBounds(300, 300, 600, 500);
+            tree = new TreeView();
+            Controls.Add(tree);
+            tree.SetBounds(300, 30, 300, 400);
+            TreeNode n1 = new TreeNode("Root node 1");
+            TreeNode n2 = new TreeNode("Image 2", 0, 1);
+            tree.Nodes.Add(n1);
+            try
+            {
+                galary = new ImageList();
+                tree.ImageList = galary;
+                galary.ImageSize = new Size(65, 65);
+                galary.ColorDepth = ColorDepth.Depth24Bit;
+                Bitmap bmp = new Bitmap("../../1.jpg");
+                galary.Images.Add(bmp);
+                bmp = new Bitmap("../../2.jpg");
+                galary.Images.Add(bmp);
+                bmp = new Bitmap("../../3.jpg");
+                galary.Images.Add(bmp);
+                bmp = new Bitmap("../../4.jpg");
+                galary.Images.Add(bmp);
+                n1 = new TreeNode("Image 1", 1, 2); // первый индекс - картинка которую видишь, второй - на какую поменяется
+                tree.Nodes.Add(n1);
+                n1.Nodes.Add(new TreeNode("Image 2", 3, 2));
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            tree.DoubleClick += new EventHandler(tree_DoubleClick);
+            lb = new ListBox();
+            lb.SetBounds(20, 200, 250, 300);
+            Controls.Add(lb);
+            all_list(tree.Nodes,"");
+
+
+            //TreeNode n2 = new TreeNode("Root node 2");
+           // tree.Nodes.Add(n2);
+        }
+
+        private void tree_DoubleClick(object sender, EventArgs e)
+        {
+            TreeView tr = (TreeView)sender;
+            tree.Nodes.Remove(tree.SelectedNode);
+        }
+
+        private void all_list(TreeNodeCollection nodes, string v)
+        {
+            foreach(TreeNode item in nodes)
+            {
+                lb.Items.Add(v + item.Text);
+                if (item.Nodes.Count > 0)
+                    all_list(item.Nodes, v + item.Text + " : ");
+            }
+        }
+    }
+}
